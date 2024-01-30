c37b719bfa75e93270e5d76b4c727ee4a2863338
WChienMing <woochienming@gmail.com>
2024-01-30 18:54:14 +0800

    a

--- app/Views/gallery.php
+++ app/Views/gallery.php
@@ -23,27 +23,171 @@
                                 <div id="collapseBackground" class="accordion-collapse collapse" aria-labelledby="headingBackground" data-bs-parent="#accordionBackground">
                                 <div class="accordion-body">
                                     <ul class="list-unstyled">
-                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="backgroundOrange">Orange</li>
-                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="backgroundRed">Red</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
                                     </ul>
                                 </div>
                                 </div>
                             </div>
                             <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingSkin">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSkin" aria-expanded="false" aria-controls="collapseBackground">
-                                    SKIN
+                                <h2 class="accordion-header" id="headingBase">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBase" aria-expanded="false" aria-controls="collapseBase">
+                                    BASE
                                 </button>
                                 </h2>
-                                <div id="collapseSkin" class="accordion-collapse collapse" aria-labelledby="headingSkin" data-bs-parent="#accordionSkin">
+                                <div id="collapseBase" class="accordion-collapse collapse" aria-labelledby="headingBase" data-bs-parent="#accordionBase">
                                 <div class="accordion-body">
                                     <ul class="list-unstyled checkbox">
-                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="SkinOrange">Orange</li>
-                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="SkinRed">Red</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
                                     </ul>
                                 </div>
                                 </div>
                             </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingBeard">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBeard" aria-expanded="false" aria-controls="collapseBeard">
+                                    BEARD
+                                </button>
+                                </h2>
+                                <div id="collapseBeard" class="accordion-collapse collapse" aria-labelledby="headingBeard" data-bs-parent="#accordionBeard">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingBlush">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBlush" aria-expanded="false" aria-controls="collapseBlush">
+                                    BLUSH
+                                </button>
+                                </h2>
+                                <div id="collapseBlush" class="accordion-collapse collapse" aria-labelledby="headingBlush" data-bs-parent="#accordionBlush">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingBrow">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseBrow" aria-expanded="false" aria-controls="collapseBrow">
+                                    BROW
+                                </button>
+                                </h2>
+                                <div id="collapseBrow" class="accordion-collapse collapse" aria-labelledby="headingBrow" data-bs-parent="#accordionBrow">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingClothes">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseClothes" aria-expanded="false" aria-controls="collapseClothes">
+                                    CLOTHES
+                                </button>
+                                </h2>
+                                <div id="collapseClothes" class="accordion-collapse collapse" aria-labelledby="headingClothes" data-bs-parent="#accordionClothes">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingEar">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEar" aria-expanded="false" aria-controls="collapseEar">
+                                    EAR
+                                </button>
+                                </h2>
+                                <div id="collapseEar" class="accordion-collapse collapse" aria-labelledby="headingEar" data-bs-parent="#accordionEar">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingEye">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEye" aria-expanded="false" aria-controls="collapseEye">
+                                    EYE
+                                </button>
+                                </h2>
+                                <div id="collapseEye" class="accordion-collapse collapse" aria-labelledby="headingEye" data-bs-parent="#accordionEye">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingHeadset">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseHeadset" aria-expanded="false" aria-controls="collapseHeadset">
+                                    HEADSET
+                                </button>
+                                </h2>
+                                <div id="collapseHeadset" class="accordion-collapse collapse" aria-labelledby="headingHeadset" data-bs-parent="#accordionHeadset">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingNose">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNose" aria-expanded="false" aria-controls="collapseNose">
+                                    NOSE
+                                </button>
+                                </h2>
+                                <div id="collapseNose" class="accordion-collapse collapse" aria-labelledby="headingNose" data-bs-parent="#accordionNose">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingMouth">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMouth" aria-expanded="false" aria-controls="collapseMouth">
+                                    MOUTH
+                                </button>
+                                </h2>
+                                <div id="collapseMouth" class="accordion-collapse collapse" aria-labelledby="headingMouth" data-bs-parent="#accordionMouth">
+                                <div class="accordion-body">
+                                    <ul class="list-unstyled checkbox">
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Orange</li>
+                                    <li><input class="form-check-input me-1" type="checkbox" value="" id="">Red</li>
+                                    </ul>
+                                </div>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                     </div>
@@ -52,8 +196,8 @@
         </div>
 
         <div class="col-lg-9">
-            <div class="title-search">
-                <div class="title-wrapper nft-h1" style="color: #000">
+            <div class="title-search pb-2">
+                <div class="title-wrapper nft-h1 m-0" style="color: #000">
                     Gallery
                 </div>
                 <div>
